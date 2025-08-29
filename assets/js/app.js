7eb7a2a4adcf4406d462f6f1e9fe21c521e50e12
munib-cmyk <munib@thepurhealth.com>
2025-08-29 11:49:06 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,19 @@
-
-
 /**
  * Theme: Crovex - Responsive Bootstrap 4 Admin Dashboard
  * Author: Mannatthemes
  * Module/App: Main Js
  */
 
-
-/*(function ($) {
-
+(function ($) {
     'use strict';
 
-    function initSlimscroll() {
-        $('.slimscroll').slimscroll({
-            height: 'auto',
-            position: 'right',
-            size: "7px",
-            color: '#a2b1d021',
-            opacity: 1,
-            wheelStep: 5,
-            touchScrollStep: 50
-        });
-    }
+    // Removed slimscroll and metisMenu (not included in assets, causing errors)
 
-   */
-   /* function initMetisMenu() {
-        //metis menu
-        $(".metismenu").metisMenu();
-        $( window ).resize(function() {
-            initEnlarge();
-        });
-    }
-*/
     function initLeftMenuCollapse() {
         // Left menu collapse
         $('.button-menu-mobile').on('click', function (event) {
             event.preventDefault();
             $("body").toggleClass("enlarge-menu");
-            initSlimscroll();
         });
     }
 
@@ -45,91 +21,68 @@
         if ($(window).width() < 1025) {
             $('body').addClass('enlarge-menu enlarge-menu-all');
         } else {
-            // if ($('body').data('keep-enlarged') != true)
-                $('body').removeClass('enlarge-menu enlarge-menu-all');
+            $('body').removeClass('enlarge-menu enlarge-menu-all');
         }
     }
 
-    function initTooltipPlugin(){
-        $.fn.tooltip && $('[data-toggle="tooltip"]').tooltip()        
+    function initTooltipPlugin() {
+        if ($.fn.tooltip) {
+            $('[data-toggle="tooltip"]').tooltip();
+        }
     }
 
     function initMainIconTabMenu() {
-        $('.main-icon-menu .nav-link').on('click', function(e){
+        $('.main-icon-menu .nav-link').on('click', function (e) {
             $("body").removeClass("enlarge-menu");
             e.preventDefault();
-            $(this).addClass('active');
-            $(this).siblings().removeClass('active');
+            $(this).addClass('active').siblings().removeClass('active');
             $('.main-menu-inner').addClass('active');
             var targ = $(this).attr('href');
-            $(targ).addClass('active');
-            $(targ).siblings().removeClass('active');
+            $(targ).addClass('active').siblings().removeClass('active');
         });
     }
 
-
     function initActiveMenu() {
-        // === following js will activate the menu in left side bar based on url ====
+        // Highlight active menu link
         $(".leftbar-tab-menu a, .left-sidenav a").each(function () {
             var pageUrl = window.location.href.split(/[?#]/)[0];
-            if (this.href == pageUrl) { 
-                $(this).addClass("active");                
-                $(this).parent().addClass("active"); // add active to li of the current link                 
-                $(this).parent().parent().addClass("in");
-                $(this).parent().parent().addClass("mm-show");
-                $(this).parent().parent().parent().addClass("mm-active");
-                $(this).parent().parent().prev().addClass("active"); // add active class to an anchor
-                $(this).parent().parent().parent().addClass("active");
-                $(this).parent().parent().parent().parent().addClass("mm-show"); // add active to li of the current link                
-                $(this).parent().parent().parent().parent().parent().addClass("mm-active");
-                var menu =  $(this).closest('.main-icon-menu-pane').attr('id');
-                $("a[href='#"+menu+"']").addClass('active');
-                
+            if (this.href === pageUrl) {
+                $(this).addClass("active");
+                $(this).parent().addClass("active");
             }
         });
     }
 
     function initFeatherIcon() {
-        feather.replace()
+        if (typeof feather !== "undefined") {
+            feather.replace();
+        }
     }
-    // Auto complate
 
     function initAutoComplate() {
-        $(document).ready(function() {
-            BindControls();
-        });
-    
-        function BindControls() {
-            var Countries = ['Forms', 
-                'Tables', 
-                'Charts', 
-                'Icones',
-                'Maps'];
-    
+        if ($.fn.autocomplete) {
+            var Countries = ['Forms', 'Tables', 'Charts', 'Icones', 'Maps'];
             $('#AllCompo').autocomplete({
                 source: Countries,
                 minLength: 0,
                 scroll: true
-            }).focus(function() {
+            }).focus(function () {
                 $(this).autocomplete("search", "");
             });
-        } 
+        }
     }
-
 
     function initMainIconMenu() {
         $(".navigation-menu a").each(function () {
             var pageUrl = window.location.href.split(/[?#]/)[0];
-            if (this.href == pageUrl) {
-                $(this).parent().addClass("active"); // add active to li of the current link
-                $(this).parent().parent().parent().addClass("active"); // add active class to an anchor
-                $(this).parent().parent().parent().parent().parent().addClass("active"); // add active class to an anchor
+            if (this.href === pageUrl) {
+                $(this).parent().addClass("active");
             }
         });
     }
 
     function initTopbarMenu() {
-        $('.navbar-toggle').on('click', function (event) {
+        $('.navbar-toggle').on('click', function () {
             $(this).toggleClass('open');
             $('#navigation').slideToggle(400);
         });
@@ -139,14 +92,14 @@
         $('.navigation-menu li.has-submenu a[href="#"]').on('click', function (e) {
             if ($(window).width() < 992) {
                 e.preventDefault();
-                $(this).parent('li').toggleClass('open').find('.submenu:first').toggleClass('open');
+                $(this).parent('li')
+                       .toggleClass('open')
+                       .find('.submenu:first').toggleClass('open');
             }
         });
     }
 
     function init() {
-        initSlimscroll();
-        initMetisMenu();
         initLeftMenuCollapse();
         initEnlarge();
         initTooltipPlugin();
@@ -156,11 +109,11 @@
         initAutoComplate();
         initMainIconMenu();
         initTopbarMenu();
-        Waves.init();
+        if (typeof Waves !== "undefined") {
+            Waves.init();
+        }
     }
 
     init();
 
-})(jQuery)
-
-
+})(jQuery);
