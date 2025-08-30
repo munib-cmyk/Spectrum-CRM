78f9cb06bec6109576201218af91d56ea7744745
munib-cmyk <munib@thepurhealth.com>
2025-08-30 00:16:53 -0700

    fix: strip flot from pipeline pages + configure Firebase API keys

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,11 +1,14 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "YOUR_REAL_API_KEY",
   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_PROJECT_ID.appspot.com",
   messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
+
+export { firebaseConfig };
 
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
